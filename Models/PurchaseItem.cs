2ff6de758dde88dccd98155bd5f73b00364646cc
natta1995 <natalie.hallerdal@yahoo.se>
2026-08-05 15:02:26 +0200

    Added Models for purchase and purchaseItem.

---
+++ Models/PurchaseItem.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SpendInsights.Models
+{
+    public class PurchaseItem
+    {
+        public int Id { get; set; }
+
+        public string ItemName { get; set; } = string.Empty;
+
+        public int Amount { get; set; } = 1;
+
+        public decimal Price { get; set; }
+
+        public double? Weight { get; set; }
+    }
+}
